ca5fd5cfc922a1ed3864d260089d5b98e496257f
juxnmxG <juxnmxgxmbox@gmail.com>
2021-09-16 20:58:29 -0500

    agregando las rutas y componente form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import FormCalc from "./components/FormCalc";
 
 function App() {
-  useEffect(() => {});
-  return (<div className="App"></div>);
+  const [data, setData] = useState([]);
+
+  const apiPost = async (name) => {
+    const url = `https://api.agify.io?name=${name}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+    });
+    const res = await response.json();
+
+    setData(res);
+
+    console.log(data);
+  };
+
+  return (
+      <Router>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link to="/" className="nav-link active">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/app" className="nav-link">App</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/" exact>
+            <FormCalc calcular={apiPost}></FormCalc>
+          </Route>
+          <Route path="/app">saasdfasdfa</Route>
+        </Switch>
+      </Router>
+    
+  );
 }
 
 export default App;
---
+++ src/components/FormCalc/FormCalc.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FormCalc() {
+  return (
+    <div>
+      <form>
+        <input type="text"></input>
+        <button type="submit">calcular</button>
+      </form>
+    </div>
+  );
+}
+
+export default FormCalc;
